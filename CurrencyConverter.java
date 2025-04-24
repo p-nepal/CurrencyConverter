7298f932a135a7d40ec4338f139e731b59baea1a
cunning6423 <cunningg4263@student.faytechcc.edu>
2025-04-24 14:35:35 -0400

    Update
    This has added all of the functionality, the only fix we need to make is a change to the UI but that would probably be all

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -59,11 +59,18 @@ public class CurrencyConverter {
         conversionRates.put("Polish Złoty", 3.79); // Polish Złoty
         conversionRates.put("New Zealand Dollar", 1.74); // New Zealand Dollar
         conversionRates.put("New Taiwan dollar", 32.79); // Polish Złoty
+        conversionRates.put("Moldovan Leu", 16.97); //Moldovan Leu
+        conversionRates.put("Macedonian Denar", 53.81); // Macedonian Denar
+        conversionRates.put("Macanese Pataca", 7.99); // Macanese Pataca
+        conversionRates.put("Laotain Kip", 21589.93); // Laotain Kip
+        conversionRates.put("Kazakhstani Tenge", 21589.93); // Kyrgyzstan
+        conversionRates.put("Indian Rupee", 85.19 ); // India, Bhutan
+        conversionRates.put("Hungarian Forint", 355.40); // Hungary
+        conversionRates.put("Georgian Lari", 2.74); //Georgia
 
         
-        String[] currencies = conversionRates.keySet().toArray(new String[0]);
-
-        Arrays.sort(currencies); // This sorts all the values by alphabetical order
+        String[] currencies = conversionRates.keySet().toArray(new String[0]); // creates a list called currencies with all the currency names
+        Arrays.sort(currencies); // this sorts all the values by alphabetical order
 
         // Components
         JComboBox<String> fromCurrency = new JComboBox<>(currencies); // creates a drop down menu with currencies
@@ -71,14 +78,16 @@ public class CurrencyConverter {
         JTextField amountField = new JTextField(); // allows you to input the desired amount
         JButton swapButton = new JButton("Swap"); // this allows you to swap the two currencies
         JButton clearButton = new JButton("Clear "); // this clears whatever you have selected
+        JButton transactionHistory = new JButton("Transactions");  // Shows the users transaction history
+        DefaultListModel<String> historyModel = new DefaultListModel<>(); // List model that does the same as above
 
         // Panel to hold components
         JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); // sets the layout, if y axis top to bottom and if x axis left to right
 
         // swap button code
         swapButton.addActionListener(new ActionListener() {
-            @Override
+            @Override // to prevent a possible silent break or not overriding properly
             public void actionPerformed(ActionEvent e) {
                 // Get selected items
                 Object from = fromCurrency.getSelectedItem();
@@ -92,14 +101,16 @@ public class CurrencyConverter {
 
         // clear button code
         clearButton.addActionListener(new ActionListener() {
-            @Override
+            @Override 
             public void actionPerformed(ActionEvent e) {
+                // Dialog box that makes sure the user wants to clear what is on the screen
                 int result = JOptionPane.showConfirmDialog(
                     null, 
                             "Are you sure you want to clear everything?", 
                             "Confirm Action", 
                             JOptionPane.YES_NO_OPTION
             );
+                // Sets the selection to ---Select Currency or the default option
                 if (result == JOptionPane.YES_OPTION) {
                     fromCurrency.setSelectedItem("---Select Currency---");
                     toCurrency.setSelectedItem("---Select Currency---");
@@ -107,49 +118,95 @@ public class CurrencyConverter {
             }
         });
 
-        // the menu screen
-        JLabel welcome = new JLabel("Welcome to the Currency Converter!");
-        welcome.setHorizontalAlignment(SwingConstants.RIGHT);
-        panel.add(welcome);
-        panel.add(new JLabel("Convert from:"));
-        panel.add(fromCurrency);
-        panel.add(new JLabel("Convert to:"));
-        panel.add(toCurrency);
-        panel.add(new JLabel("Switch: "));
-        panel.add(swapButton);
-        panel.add(new JLabel("Clear: "));
-        panel.add(clearButton);
-        panel.add(new JLabel("Enter amount:"));
-        panel.add(amountField);
-
-        int result = JOptionPane.showConfirmDialog(null, panel, 
-                 "Currency Converter Menu", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-
-        
-        if (result == JOptionPane.OK_OPTION) {
-            try {
-                String from = (String) fromCurrency.getSelectedItem();
-                String to = (String) toCurrency.getSelectedItem();
-                double amount = Double.parseDouble(amountField.getText());
-
-                double fromRate = conversionRates.get(from);
-                double toRate = conversionRates.get(to);
-
-                if (fromRate == 0 || toRate == 0) {
-                    JOptionPane.showMessageDialog(
-                                null,
-                                "You need to select two currencies",
-                                "Error",
-                                JOptionPane.ERROR_MESSAGE);
+        // transaction button code
+        transactionHistory.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // if there has been no transactions then it will display a message instead of just an empty dialog box
+                if (historyModel.size() < 1) {
+                    JOptionPane.showMessageDialog(null, "You have not completed any transactions yet.", "Transaction History", JOptionPane.INFORMATION_MESSAGE);
                 } else {
-                    // Formula: amount * (toRate / fromRate)
-                double converted = amount * (toRate / fromRate);
-
-                JOptionPane.showMessageDialog(null,
-                        String.format("%.2f %s = %.2f %s", amount, from, converted, to));
+                    // this displays the transaction history if they have 1 or more transactions in their history
+                    StringBuilder historyText = new StringBuilder(); // class in JOptionPane used to help us build these strings which eventually go on the list. 
+                    for (int i = 0; i < historyModel.size(); i++) {
+                        historyText.append(historyModel.getElementAt(i)).append("\n"); // adds whatever the last transaction was
+                    }
+                    JOptionPane.showMessageDialog(null, historyText.toString(), "Transaction History", JOptionPane.INFORMATION_MESSAGE);
                 }
-            } catch (NumberFormatException e) {
-                JOptionPane.showMessageDialog(null, "Please enter a valid number.");
+            }
+            
+        });
+
+        // the menu screen 
+        // JLabels so people are aware what they would need to input to properly use the program
+        panel.add(new JLabel("Currency Converter"));
+        panel.add(new JLabel("Convert from:"));
+        panel.add(fromCurrency); // JComboBox
+        panel.add(new JLabel("Convert to:"));
+        panel.add(toCurrency); // JComboBox
+        panel.add(new JLabel("Switch: "));
+        panel.add(swapButton); // JButton 
+        panel.add(new JLabel("Clear: "));
+        panel.add(clearButton); // JButton 
+        panel.add(new JLabel("Enter amount:"));
+        panel.add(amountField); // JTextField
+        panel.add(new JLabel("Transactions"));
+        panel.add(transactionHistory); // JButton 
+
+            
+        // the while loop
+        boolean stop = false;
+        while (!stop) {            
+            // the menu screen
+            int result = JOptionPane.showConfirmDialog(null, panel, 
+                    "Currency Converter Menu", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+            // allows the user to cancel at the main menu and at the cancel screen
+            if (result == JOptionPane.CANCEL_OPTION) {
+                int exit = JOptionPane.showConfirmDialog(null, 
+                    "Are you sure you want to leave the program?", "Currency Converter", JOptionPane.OK_OPTION, JOptionPane.PLAIN_MESSAGE);
+                if (exit == JOptionPane.OK_OPTION) {
+                    JOptionPane.showMessageDialog(null,"Thank you for using this program!");
+                    break;
+                }
+            }
+            // 
+            if (result == JOptionPane.OK_OPTION) {
+                try {
+                    String from = (String) fromCurrency.getSelectedItem();
+                    String to = (String) toCurrency.getSelectedItem();
+                    double amount = Double.parseDouble(amountField.getText());
+
+                    double fromRate = conversionRates.get(from);
+                    double toRate = conversionRates.get(to);
+
+                    if (fromRate == 0 || toRate == 0) {
+                        JOptionPane.showMessageDialog(
+                                    null,
+                                    "You need to select two currencies",
+                                    "Error",
+                                    JOptionPane.ERROR_MESSAGE);
+                    } else {
+                        // Formula: amount * (toRate / fromRate)
+                    double converted = amount * (toRate / fromRate);
+
+                    JOptionPane.showMessageDialog(null,
+                            String.format("%.2f %s = %.2f %s", amount, from, converted, to));
+                            String logEntry = amount + " " + from + " = " + converted + " " + to;
+                            historyModel.addElement(logEntry);
+                    int stopChoice = JOptionPane.showConfirmDialog(null, 
+                    "Would you like to run the program again?", "Currency Converter", JOptionPane.OK_OPTION, JOptionPane.PLAIN_MESSAGE);
+                        if (stopChoice == JOptionPane.OK_OPTION) {
+                            continue;
+                        }
+                        else {
+                            JOptionPane.showMessageDialog(null,"Thank you for using this program!");
+                            break;
+                        } 
+                    }
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Please enter a valid number.");
+                }
             }
         }
     }
